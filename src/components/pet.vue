213ac36d18db8954e532227a21312350e99e0575
lordspelde <rwspelde15@gmail.com>
2026-05-03 18:26:54 -0500

    database support

--- src/components/pet.vue
+++ src/components/pet.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+
 const props = defineProps({
   name: { type: String, required: true }, // displayed on the page
   id: { type: String, required: true }, // WIP / TODO used for querying the rest of the details from a TBD server
@@ -9,18 +11,31 @@ const props = defineProps({
 //   descriptors: { type: Array, default: () => ['N/A'] },
 })
 
-// grab specific pet
-const response = fetch(`/api/pets/${props.name}`)
+const images = ref([])
+const features = ref('N/A')
+const descriptors = ref('N/A')
+const loading = ref(true)
 
-if (!response) {
-  console.error(`Failed to fetch pet: ${props.name}`)
-}
+onMounted(async () => {
+  try {
+    // grab specific pet
+    const response = await fetch(`http://localhost:3000/pets/${props.id}`)
 
-const petData = await response.json()
+    if (!response.ok) {
+      console.error(`Failed to fetch pet: ${props.name}`)
+    }
 
-const images = petData.images || []
-const features = petData.features || 'N/A'
-const descriptors = petData.descriptors || 'N/A'
+    const petData = await response.json()
+
+    images.value = petData.photos || []
+    features.value = petData.features || 'N/A'
+    descriptors.value = petData.descriptors || 'N/A'
+  } catch (error) {
+    console.error(`Error fetching pet data for ${props.name}`, error)
+  } finally {
+    loading.value = false
+  }
+})
 </script>
 
 <template>
@@ -29,8 +44,8 @@ const descriptors = petData.descriptors || 'N/A'
       <v-col cols="12" md="8">
         <v-card elevation="2" class="rounded-xl overflow-hidden">
           <v-card-title class="text-h3 ml-4 mb-4">{{ name }}</v-card-title>
-          <v-carousel crossfade="true" cycle="true" hide-delimiters="true">
-            <v-carousel-item v-for="img in images" src="img"/>
+          <v-carousel crossfade=true cycle=true hide-delimiters=true>
+            <v-carousel-item v-for="img in images" :src="img"/>
           </v-carousel>
 
           <v-card-text>
