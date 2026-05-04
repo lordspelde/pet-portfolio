f3db9281e843f30178411fcc7c78ff907ef7d0d3
lordspelde <rwspelde15@gmail.com>
2026-05-03 19:36:32 -0500

    Added photo gallery

--- src/components/pet.vue
+++ src/components/pet.vue
@@ -11,10 +11,21 @@ const props = defineProps({
 //   descriptors: { type: Array, default: () => ['N/A'] },
 })
 
+const gridVisible = ref(false)
+const uploadVisible = ref(false)
+
 const images = ref([])
 const features = ref('N/A')
 const descriptors = ref('N/A')
 const loading = ref(true)
+
+function toggleGrid() {
+  gridVisible.value = !gridVisible.value
+}
+
+function toggleUpload() {
+  uploadVisible.value = !uploadVisible.value
+}
 
 onMounted(async () => {
   try {
@@ -39,31 +50,111 @@ onMounted(async () => {
 </script>
 
 <template>
+  <!-- Main card -->
   <v-container>
-    <v-row justify="center">
-      <v-col cols="12" md="8">
-        <v-card elevation="2" class="rounded-xl overflow-hidden">
-          <v-card-title class="text-h3 ml-4 mb-4">{{ name }}</v-card-title>
-          <v-carousel crossfade=true cycle=true hide-delimiters=true>
-            <v-carousel-item v-for="img in images" :src="img"/>
-          </v-carousel>
+    <v-card
+      elevation="2"
+      class="rounded-xl overflow-hidden mx-auto"
+      max-width="40%"
+    >
+      <v-card-title class="text-h3 ml-4 mb-4">{{ name }}</v-card-title>
+      <v-carousel crossfade=true cycle=true hide-delimiters=true :key="images.length">
+        <v-carousel-item v-for="img in images" :src="img"/>
+      </v-carousel>
 
-          <v-card-text>
-            <h4>About me</h4>
-            <p class="text-body-1">{{ descriptors }}</p>
-            <h4>Features</h4>
-            <p class="text-body-1">{{ features }}</p>
-          </v-card-text>
+      <v-card-text>
+        <h4>About me</h4>
+        <p class="text-body-1">{{ descriptors }}</p>
+        <h4>Features</h4>
+        <p class="text-body-1">{{ features }}</p>
+      </v-card-text>
 
-          <v-card-actions>
-            <v-btn color="primary" variant="tonal" block size="large">
-              See more of {{ name }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn color="primary" variant="tonal" size="large" @click="toggleGrid">
+          See more of {{ name }}
+        </v-btn>
+        <v-btn color="secondary" variant="tonal" size="large" @click="toggleUpload">
+          Upload additional photos
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+
+  <!-- Photo grid -->
+  <v-dialog
+    v-model="gridVisible"
+    width="85%"
+    height="85%"
+    @click:outside="toggleGrid"
+    transition="dialog-bottom-transition"
+    :scrim="false"
+  >
+    <v-card>
+      <v-toolbar color="transparent" class="text-white px-4">
+        <v-toolbar-title>Photo Gallery</v-toolbar-title>
+        <v-spacer/>
+        <v-btn icon="mdi-close" variant="text" @click="gridVisible = false"/>
+      </v-toolbar>
+
+      <v-card-text>
+        <v-container fluid class="masonry-grid">
+          <v-hover
+            v-slot="{ isHovering, props }"
+            v-for="(img, i) in images"
+            :key="i"
+          >
+            <v-card
+              v-bind="props"
+              :elevation="isHovering ? 12 : 2"
+              class="rounded-lg overflow-hidden masonry-item"
+            >
+              <v-img :src="img"/>
+            </v-card>
+          </v-hover>
+        </v-container>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+
+  <!-- Upload additional photos -->
+  <v-dialog v-model="uploadVisible">
+    <v-card>
+      <v-card-title>Upload Photo</v-card-title>
+      <v-card-text>
+        <p>Upload additional photos of {{ name }}</p>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+
+  <!-- Loading indicator -->
+  <v-container v-if="loading" class="text-center">
+    <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    <p>Loading pet details...</p>
   </v-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+.masonry-grid {
+  column-count: 6;
+  gap: 16px;
+  width: 100%;
+}
+
+.masonry-item {
+  display: inline-block;
+  width: 100%;
+}
+
+@media (max-width: 1200px) {
+  .masonry-grid {
+    column-count: 4;
+  }
+}
+
+@media (max-width: 600px) {
+  .masonry-grid {
+    column-count: 3;
+  }
+}
+</style>
